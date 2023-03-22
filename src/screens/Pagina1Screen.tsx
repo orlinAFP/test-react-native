d9d752163e60d99e395fdf0d2cb1b6f22ff8f67c
Orlin Williams Gómez Gómez <owg147@ADPensiones.com>
2023-03-21 19:36:55 -0600

    iniciando

---
+++ src/screens/Pagina1Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export const Pagina1Screen = () => {
+  return (
+    <View>
+      <Text>App Screen 1</Text>
+    </View>
+  );
+};
---
+++ src/screens/Pagina2Screen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export const Pagina2Screen = () => {
+  return (
+    <View>
+
+
+    </View>
+  );
+};
